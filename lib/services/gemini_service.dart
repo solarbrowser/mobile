ceaa3825325ac35aa0ccb351245ab39a432b32a6
Ata Türkçü <68029503+ataturkcu@users.noreply.github.com>
2025-07-05 22:05:12 +0300

    update: threshold update

--- lib/services/gemini_service.dart
+++ lib/services/gemini_service.dart
@@ -60,19 +60,19 @@ class GeminiService {
           'safetySettings': [
             {
               'category': 'HARM_CATEGORY_DANGEROUS_CONTENT',
-              'threshold': 'BLOCK_HIGH'
+              'threshold': 'BLOCK_LOW_AND_ABOVE'
             },
             {
               'category': 'HARM_CATEGORY_HATE_SPEECH',
-              'threshold': 'BLOCK_HIGH'
+              'threshold': 'BLOCK_LOW_AND_ABOVE'
             },
             {
               'category': 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
-              'threshold': 'BLOCK_HIGH'
+              'threshold': 'BLOCK_LOW_AND_ABOVE'
             },
             {
               'category': 'HARM_CATEGORY_HARASSMENT',
-              'threshold': 'BLOCK_HIGH'
+              'threshold': 'BLOCK_LOW_AND_ABOVE'
             }
           ],
           'generationConfig': {
